03c1b1f7cb8a6a013ce918bfa4dee923aa9da26f
jayson7 <lexxiijoo70@gmail.com>
2022-05-24 14:04:08 +0100

    first commit

---
+++ Main.java
@@ -0,0 +1,9 @@
+public class Main {
+
+  public static void main(String[] args) {
+    System.out.println("Hello World");
+    System.out.println(32 + 67);
+    // this is a single line comment
+
+  }
+}
