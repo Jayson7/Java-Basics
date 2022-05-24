5f16c7c99c09038a8706aa7bf75f5bd243f57cfe
jayson7 <lexxiijoo70@gmail.com>
2022-05-24 14:17:27 +0100

    done

--- Main.java
+++ Main.java
@@ -4,6 +4,28 @@ public class Main {
     System.out.println("Hello World");
     System.out.println(32 + 67);
     // this is a single line comment
+    /*
+this is a multi-line comment 
+*/
 
+    // primative data types
+    // int, double, float, char, boolean, long, short, byte
+    int myInt = 5;
+    double myDouble = 5.5;
+    float myFloat = 5.5f;
+    char myChar = 'a';
+    boolean myBoolean = true;
+    long myLong = 5L;
+    short myShort = 5;
+    byte myByte = 5;
+    // print all variables
+    System.out.println(myInt);
+    System.out.println(myDouble);
+    System.out.println(myFloat);
+    System.out.println(myChar);
+    System.out.println(myBoolean);
+    System.out.println(myLong);
+    System.out.println(myShort);
+    System.out.println(myByte);
   }
 }
